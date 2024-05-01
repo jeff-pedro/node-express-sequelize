87e74b8dd129b8c4318e0abfddb082e32e037963
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-05-01 12:08:54 -0300

    Refatora a rota de pessoas

--- src/router/pessoasRoute.js
+++ src/router/pessoasRoute.js
@@ -1,13 +1,15 @@
 const { Router } = require('express');
 const PessoaController = require('../controllers/PessoaController.js'); 
 
+const pessoaController = new PessoaController( );
+
 const routes = Router();
 
 routes
-  .get('/pessoas', PessoaController.pegaTodas)
-  .get('/pessoas/:id', PessoaController.listaPessoaPorId)
-  .post('/pessoas', PessoaController.criaPessoa)
-  .put('/pessoas/:id', PessoaController.atualizaPessoa)
-  .delete('/pessoas/:id', PessoaController.excluiPessoa);
+  .get('/pessoas', (req, res) => pessoaController.pegaTodos(req, res))
+  .get('/pessoas/:id', (req, res) => pessoaController.pegaUmPorId(req, res))
+  .post('/pessoas', (req, res) => pessoaController.criaNovo(req, res))
+  .put('/pessoas/:id', (req, res) => pessoaController.atualiza(req, res))
+  .delete('/pessoas/:id', (req, res) => pessoaController.exclui(req, res));
 
 module.exports = routes;
